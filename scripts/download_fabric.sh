e5f29662cc08642515cbb7f355f4dbf7733b4ae9
christoffer.f.r <lakrestofer@gmail.com>
2023-10-21 17:41:26 +0200

    update to use version 20.1

--- scripts/download_fabric.sh
+++ scripts/download_fabric.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -OJ https://meta.fabricmc.net/v2/versions/loader/1.20.2/0.14.23/0.11.2/server/jar
+curl -OJ https://meta.fabricmc.net/v2/versions/loader/1.20.1/0.14.23/0.11.2/server/jar
--- scripts/launch.sh
+++ scripts/launch.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -Xmx2G -jar fabric-server-mc.1.20.2-loader.0.14.23-launcher.0.11.2.jar nogui
+java -Xmx2G -jar fabric-server-mc.1.20.1-loader.0.14.23-launcher.0.11.2.jar nogui
